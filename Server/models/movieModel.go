ce0cc4696415c810b42372343b28fe708a3cf0ec
deepakdev9600 <deepakdev9600@gmail.com>
2025-11-16 20:57:58 +0530

    change primitve to bson

--- Server/models/movieModel.go
+++ Server/models/movieModel.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type Genre struct {
@@ -15,12 +15,12 @@ type Ranking struct {
 }
 
 type Movie struct {
-	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	ImdbID      string             `bson:"imdb_id" json:"imdb_id" validate:"required"`
-	Title       string             `bson:"title" json:"title" validate:"required,min=2,max=500"`
-	PosterPath  string             `bson:"poster_path" json:"poster_path" validate:"required,url"`
-	YouTubeID   string             `bson:"youtube_id" json:"youtube_id" validate:"required"`
-	Genre       []Genre            `bson:"genre" json:"genre" validate:"required,dive"`
-	AdminReview string             `bson:"admin_review" josn:"admin_review" validate:"required"`
-	Ranking     Ranking            `bson:"ranking" json:"ranking" validate:"required"`
+	ID          bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	ImdbID      string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
+	Title       string        `bson:"title" json:"title" validate:"required,min=2,max=500"`
+	PosterPath  string        `bson:"poster_path" json:"poster_path" validate:"required,url"`
+	YouTubeID   string        `bson:"youtube_id" json:"youtube_id" validate:"required"`
+	Genre       []Genre       `bson:"genre" json:"genre" validate:"required,dive"`
+	AdminReview string        `bson:"admin_review" josn:"admin_review" validate:"required"`
+	Ranking     Ranking       `bson:"ranking" json:"ranking" validate:"required"`
 }
