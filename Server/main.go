ac8008ebf25ad136a60e1a6ee72750cd8d8eb24f
deepakdev9600 <deepakdev9600@gmail.com>
2025-11-16 19:21:35 +0530

    add movies route

--- Server/main.go
+++ Server/main.go
@@ -6,15 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/Deepakpmk007/movie-stream-go/controllers"
+	"github.com/Deepakpmk007/movie-stream-go/database"
 )
 
 func main() {
+	database.InitDB()
 	router := gin.Default()
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Deepak")
 	})
 
 	router.GET("/movies", controllers.GetMovies())
+	router.GET("/movies/:imdb_id", controllers.GetMovie())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
