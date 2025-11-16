1b73bf44ec94e399309b1d8427d592a648283139
deepakdev9600 <deepakdev9600@gmail.com>
2025-11-16 20:54:34 +0530

    add primitive

--- Server/models/movieModel.go
+++ Server/models/movieModel.go
@@ -1,12 +1,12 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Genre struct {
 	GenreId   int    `bson:"genre_id" json:"genre_id" validate:"required"`
-	GenreName string `bson:"genre_name" json:"genre_name" validate:"required, min=2, max=100"`
+	GenreName string `bson:"genre_name" json:"genre_name" validate:"required,min=2,max=100"`
 }
 
 type Ranking struct {
@@ -15,12 +15,12 @@ type Ranking struct {
 }
 
 type Movie struct {
-	ID          bson.ObjectID `bson:"_id" json:"_id"`
-	ImdbID      string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
-	Title       string        `bson:"title" json:"title" validate:"required,min=2, max=500"`
-	PosterPath  string        `bson:"poster_path" json:"poster_path" validate:"required, url"`
-	YouTubeID   string        `bson:"youtube_id" json:"youtube_id" validate:"required"`
-	Genre       []Genre       `bson:"genre" json:"genre" validate:"required, dive"`
-	AdminReview string        `bson:"admin_review" josn:"admin_review" validate:"required"`
-	Ranking     Ranking       `bson:"ranking" json:"ranking" validate:"required"`
+	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	ImdbID      string             `bson:"imdb_id" json:"imdb_id" validate:"required"`
+	Title       string             `bson:"title" json:"title" validate:"required,min=2,max=500"`
+	PosterPath  string             `bson:"poster_path" json:"poster_path" validate:"required,url"`
+	YouTubeID   string             `bson:"youtube_id" json:"youtube_id" validate:"required"`
+	Genre       []Genre            `bson:"genre" json:"genre" validate:"required,dive"`
+	AdminReview string             `bson:"admin_review" josn:"admin_review" validate:"required"`
+	Ranking     Ranking            `bson:"ranking" json:"ranking" validate:"required"`
 }
