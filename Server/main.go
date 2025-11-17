0f399a7eb2fcac1c8d079894df7f10315a107bd5
deepakdev9600 <deepakdev9600@gmail.com>
2025-11-17 15:51:02 +0530

    add register route

--- Server/main.go
+++ Server/main.go
@@ -19,6 +19,7 @@ func main() {
 	router.GET("/movies", controllers.GetMovies())
 	router.GET("/movies/:imdb_id", controllers.GetMovie())
 	router.POST("/addMovie", controllers.AddMovie())
+	router.POST("/register", controllers.Registeruser())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
